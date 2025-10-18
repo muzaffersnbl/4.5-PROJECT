fed63f4468e15b53c2cc8eb67743095b8d01f147
Muzaffer Sunbul <muzaffer931@hotmail.com>
2025-10-18 17:01:02 +0300

    Update application.yml to enhance management endpoint exposure settings and improve overall configuration clarity

---
+++ gateway-server/src/main/java/com/turkcell/gatway_server/GatwayServerApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.gatway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayServerApplication.class, args);
+	}
+
+}
